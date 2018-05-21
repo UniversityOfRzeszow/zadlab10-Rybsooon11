1de1c305851332c3a144b421836ff7e8bfaaeca8
twork <twork@DESKTOP-VSSKDKJ>
2018-05-21 12:59:57 +0200

    Lab10

--- OOPL10/src/main/java/pl/edu/ur/oopl10/Main.java
+++ OOPL10/src/main/java/pl/edu/ur/oopl10/Main.java
@@ -1,5 +1,5 @@
 package pl.edu.ur.oopl10;
-
+import java.util.Random;
 /**
  *
  * @author mchla
@@ -8,6 +8,23 @@ public class Main {
 
     public static void main(String[] args) {
         
+        Random rand = new Random();
+        int counter  = 0;
+        while(counter != 3) {
+            int randomNum1 = -10 + rand.nextInt((10 - -10) + 1);
+            int randomNum2 = -10 + rand.nextInt((10 - -10) + 1);
+            if(randomNum2 == 0) {
+                counter++;
+                System.out.println("Dzielnie przez zero jest niemożliwe " + counter);
+            } else {
+              double divide = randomNum1/randomNum2; 
+              System.out.println("Wynik dzielenia" + divide);
+            }
+        }
+        if(counter == 3) {
+            System.out.println("3 wystapienia");
+            throw new ArithmeticException();
+        }
     }
     
 }
---
+++ OOPL10/src/main/java/pl/edu/ur/oopl10/WprowadzZKonsoli.java
@@ -0,0 +1,175 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.ur.oopl10;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ *
+ * @author twork
+ */
+public class WprowadzZKonsoli {
+    
+     static public void wprowadzInt() {
+        Scanner input = new Scanner(System.in); 
+         try {
+            System.out.println("Podaj int");
+            int test = input.nextInt();
+            FileReader fr = new FileReader("test.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String output = br.readLine();
+            br.close();
+            FileWriter fwo = new FileWriter("test.txt");
+            BufferedWriter bwo = new BufferedWriter(fwo);
+            bwo.write(test);
+            bwo.newLine();
+            bwo.close();
+        } catch (FileNotFoundException z) {
+            System.out.print("Wyjątek  braku pliku ");
+            System.out.println(z.getMessage());
+        } catch (Exception w) {
+            System.out.print("Wyjątek: ");
+            System.out.println(w.getMessage());
+        }
+    }
+    static public void wprowadzChar() {
+        Scanner input = new Scanner(System.in); 
+         try {
+             System.out.println("Podaj char");
+           char test = input.next().charAt(0);
+            FileReader fr = new FileReader("test.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String output = br.readLine();
+            br.close();
+            FileWriter fwo = new FileWriter("test.txt");
+            BufferedWriter bwo = new BufferedWriter(fwo);
+            bwo.write(test);
+            bwo.newLine();
+            bwo.close();
+        } catch (FileNotFoundException z) {
+            System.out.print("Wyjątek  braku pliku ");
+            System.out.println(z.getMessage());
+        } catch (Exception w) {
+            System.out.print("Wyjątek: ");
+            System.out.println(w.getMessage());
+        }
+    }
+    static public void wprowadzString() {
+        Scanner input = new Scanner(System.in); 
+         try {
+             System.out.println("Podaj string");
+           String test = input.nextLine();
+            FileReader fr = new FileReader("test.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String output = br.readLine();
+            br.close();
+            FileWriter fwo = new FileWriter("test.txt");
+            BufferedWriter bwo = new BufferedWriter(fwo);
+            bwo.write(test);
+            bwo.newLine();
+            bwo.close();
+        } catch (FileNotFoundException z) {
+            System.out.print("Wyjątek  braku pliku ");
+            System.out.println(z.getMessage());
+        } catch (Exception w) {
+            System.out.print("Wyjątek: ");
+            System.out.println(w.getMessage());
+        }
+    }
+    static public void wprowadzLong() {
+        Scanner input = new Scanner(System.in); 
+         try {
+             System.out.println("Podaj long");
+           long test = input.nextLong();
+            FileReader fr = new FileReader("test.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String output = br.readLine();
+            br.close();
+            FileWriter fwo = new FileWriter("test.txt");
+            BufferedWriter bwo = new BufferedWriter(fwo);
+            bwo.write((int) test);
+            bwo.newLine();
+            bwo.close();
+        } catch (FileNotFoundException z) {
+            System.out.print("Wyjątek  braku pliku ");
+            System.out.println(z.getMessage());
+        } catch (Exception w) {
+            System.out.print("Wyjątek: ");
+            System.out.println(w.getMessage());
+        }
+    }
+    static public void wprowadzShort() {
+        Scanner input = new Scanner(System.in); 
+         try {
+             System.out.println("Podaj short");
+           short test = input.nextShort();
+            FileReader fr = new FileReader("test.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String output = br.readLine();
+            br.close();
+            FileWriter fwo = new FileWriter("test.txt");
+            BufferedWriter bwo = new BufferedWriter(fwo);
+            bwo.write(test);
+            bwo.newLine();
+            bwo.close();
+        } catch (FileNotFoundException z) {
+            System.out.print("Wyjątek  braku pliku ");
+            System.out.println(z.getMessage());
+        } catch (Exception w) {
+            System.out.print("Wyjątek: ");
+            System.out.println(w.getMessage());
+        }
+    }
+    static public void wprowadzFloat() {
+        Scanner input = new Scanner(System.in); 
+         try {
+             System.out.println("Podaj float");
+          float test = input.nextFloat();
+            FileReader fr = new FileReader("test.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String output = br.readLine();
+            br.close();
+            FileWriter fwo = new FileWriter("test.txt");
+            BufferedWriter bwo = new BufferedWriter(fwo);
+            bwo.write((int) test);
+            bwo.newLine();
+            bwo.close();
+        } catch (FileNotFoundException z) {
+            System.out.print("Wyjątek  braku pliku ");
+            System.out.println(z.getMessage());
+        } catch (Exception w) {
+            System.out.print("Wyjątek: ");
+            System.out.println(w.getMessage());
+        }
+    }
+    static public void wprowadzDouble() {
+        Scanner input = new Scanner(System.in); 
+         try {
+             System.out.println("Podaj double");
+          double test = input.nextDouble();
+            FileReader fr = new FileReader("test.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String output = br.readLine();
+            br.close();
+            FileWriter fwo = new FileWriter("test.txt");
+            BufferedWriter bwo = new BufferedWriter(fwo);
+            bwo.write((int) test);
+            bwo.newLine();
+            bwo.close();
+        } catch (FileNotFoundException z) {
+            System.out.print("Wyjątek  braku pliku ");
+            System.out.println(z.getMessage());
+        } catch (Exception w) {
+            System.out.print("Wyjątek: ");
+            System.out.println(w.getMessage());
+        }
+    }
+    
+}
